4fa23cefa26fdea8dfcd4f0f19e8d4847a59a2a5
Andreas Fink <af@andreasfink.com>
2014-07-18 12:32:14 +0200

    updated jquery 1.x and 2.x to current release

--- src/main/java/org/got5/tapestry5/jquery/JQueryVersion.java
+++ src/main/java/org/got5/tapestry5/jquery/JQueryVersion.java
@@ -9,9 +9,9 @@ public class JQueryVersion {
     /**
      * Compatible with older IEs, default.
      */
-    public static final String CORE_1_10_2 = "1.10.2";
+    public static final String CORE_1_x = "1.11.1";
 
-    public static final String CORE_2_0_3 = "2.0.3";
+    public static final String CORE_2_x = "2.1.1";
 
     public static final String UI_1_10_3 = "1.10.3";
 }
